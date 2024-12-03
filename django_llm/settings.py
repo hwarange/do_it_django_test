87086a4e900f9ba6e6be27979590c101813d34c5
hwarange <asdf78964@naver.com>
2024-12-03 16:01:48 +0900

    commit

--- django_llm/settings.py
+++ django_llm/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "crispy_forms",
     "crispy_bootstrap5",
     "markdownx",
+    'django.contrib.sites',
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
--- django_llm/urls.py
+++ django_llm/urls.py
@@ -22,9 +22,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("markdownx/", include('markdownx.urls')),
-    # path("accounts/", include('allauth.urls')),
+    path("markdownx/", include('markdownx.urls')),
+    path("accounts/", include('allauth.urls')),
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
